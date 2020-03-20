d2a216e4f814b434d320c77b8bb66982b33b419a
Richard Yu <yurichard3839@gmail.com>
2020-03-21 01:28:10 +0800

    Update messages.pot

--- translate/gen_pot.sh
+++ translate/gen_pot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FILES=""
+FILES="ClashW.cpp EdgeWebView2.hpp MenuUtil.hpp OSLicensesDlg.hpp"
 
 cd ..
 xgettext -o translate/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 $FILES
