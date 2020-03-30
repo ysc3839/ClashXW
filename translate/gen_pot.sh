c318cddd3127a67921a9d3cf18e49e509eb29146
Richard Yu <yurichard3839@gmail.com>
2020-03-30 17:51:44 +0800

    Move translate/*.po? to translate/source

--- translate/gen_pot.sh
+++ translate/gen_pot.sh
@@ -6,4 +6,4 @@ COPYRIGHT_HOLDER="Richard Yu <yurichard3839@gmail.com>"
 PKG_NAME="ClashW"
 
 cd ..
-xgettext -o translate/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
+xgettext -o translate/source/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
