817a6e606d9f33f36566535e239583755a8f01fb
Richard Yu <yurichard3839@gmail.com>
2020-03-30 17:43:51 +0800

    Add copyright holder to .pot

--- translate/gen_pot.sh
+++ translate/gen_pot.sh
@@ -2,5 +2,8 @@
 
 FILES="ClashW.cpp EdgeWebView2.hpp MenuUtil.hpp OSLicensesDlg.hpp"
 
+COPYRIGHT_HOLDER="Richard Yu <yurichard3839@gmail.com>"
+PKG_NAME="ClashW"
+
 cd ..
-xgettext -o translate/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 $FILES
+xgettext -o translate/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
