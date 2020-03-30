dd63f7c34ede68bb2de6c1c5c9c4434a84b81720
Richard Yu <yurichard3839@gmail.com>
2020-03-30 22:33:48 +0800

    Update gen_pot.sh

--- translate/gen_pot.sh
+++ translate/gen_pot.sh
@@ -5,5 +5,7 @@ FILES="ClashW.cpp EdgeWebView2.hpp MenuUtil.hpp OSLicensesDlg.hpp"
 COPYRIGHT_HOLDER="Richard Yu <yurichard3839@gmail.com>"
 PKG_NAME="ClashW"
 
+pipenv run rc2po --pot --charset=utf-16le --lang=LANG_ENGLISH --sublang=SUBLANG_ENGLISH_US ./rc/ClashW-translatable.rc ./rc/resource.pot
+
 cd ..
 xgettext -o translate/source/messages.pot --c++ --add-comments=/ --keyword=_ --keyword=C_:1c,2 --copyright-holder="$COPYRIGHT_HOLDER" --package-name="$PKG_NAME" $FILES
