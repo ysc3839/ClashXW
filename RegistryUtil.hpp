67fc23a897ab89541bbcc0061facde3002d36a67
Richard Yu <yurichard3839@gmail.com>
2020-10-21 01:43:36 +0800

    Fix wstring_view nullptr issue

--- RegistryUtil.hpp
+++ RegistryUtil.hpp
@@ -4,7 +4,12 @@ namespace RegUtil
 {
 	inline auto RegSetStringValue(HKEY hKey, LPCWSTR lpValueName, std::wstring_view str)
 	{
-		return RegSetValueExW(hKey, lpValueName, 0, REG_SZ, reinterpret_cast<const BYTE*>(str.data()), str.data() ? static_cast<DWORD>((str.size() + 1) * sizeof(wchar_t)) : 0);
+		return RegSetValueExW(hKey, lpValueName, 0, REG_SZ, reinterpret_cast<const BYTE*>(str.data()), static_cast<DWORD>((str.size() + 1) * sizeof(wchar_t)));
+	}
+
+	inline auto RegSetStringValue(HKEY hKey, LPCWSTR lpValueName, std::nullptr_t)
+	{
+		return RegSetValueExW(hKey, lpValueName, 0, REG_SZ, nullptr, 0);
 	}
 
 	inline auto RegSetDWORDValue(HKEY hKey, LPCWSTR lpValueName, DWORD value)
